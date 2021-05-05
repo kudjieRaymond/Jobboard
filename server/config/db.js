ee58ef401bf35900681e7ec136ac5be46e9284c9
Ray Kudjie <rkudjie@gmail.com>
2021-05-06 00:04:35 +0200

    update .env

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
-	url:
-		"mongodb+srv://admin:elle@cluster0-kg1mb.mongodb.net/jobboard?retryWrites=true&w=majority",
+	url: process.env.MONGO_DB_URL,
 };
